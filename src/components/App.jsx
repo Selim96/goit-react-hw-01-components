e4acd2e8968402e2a118bf5616a6b626856574aa
Selim Fazylov <selim.fazylov1@gmail.com>
2022-04-09 00:22:08 +0300

    render 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,26 @@
+import Profile from "./profile/Profile";
+// import user from "../";
+// console.log(user);
+
+const data = {
+  "username": "Jacques Gluke",
+  "tag": "jgluke",
+  "location": "Ocho Rios, Jamaica",
+  "avatar": "https://cdn-icons-png.flaticon.com/512/2922/2922506.png",
+  "stats": {
+    "followers": 5603,
+    "views": 4827,
+    "likes": 1308
+  }
+}
+
+const json = JSON.stringify(data);
+const user = JSON.parse(json);
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        textTransform: 'uppercase',
-        color: '#010101',
-      }}
-    >
-      React homework template
+    <div>
+      <Profile username={user.username} avatar={user.avatar} tag={user.tag} location={user.location} stats={user.stats}/>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from "react-dom/client";
 import { App } from 'components/App';
-// import './index.css';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+
+const elem = (
+  <>
+    <div>
+      <App />
+    </div>
+  </>);
+
+console.log(elem);
+const container = document.getElementById('root');
+ReactDOM.createRoot(container).render(elem);
+
+
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
